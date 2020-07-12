bda0bb025043fb93c33024e9354d246bef9dea43
boseji <boseji@users.noreply.github.com>
2020-07-12 10:21:50 +0530

    Better coverage - You are doing Great ! :star2:

--- serial_test.go
+++ serial_test.go
@@ -1,7 +1,10 @@
 package goembserial
 
 import (
+	"bytes"
 	"errors"
+	"fmt"
+	"math/rand"
 	"os"
 	"strconv"
 	"testing"
@@ -588,4 +591,256 @@ func TestSerialIntegration_P13(t *testing.T) {
 	val, err = handle.Dsr()
 	assert.NoError(t, err)
 	assert.Equal(t, false, val)
+}
+
+func TestSerialIntegration_P14(t *testing.T) {
+
+	verifySetup(t, paramLOOPBACK)
+
+	handle, err := createPort(t, ParityNone, StopBits1, FlowNone)
+	defer closePort(t, handle)
+
+	// Flip Polarity
+	handle.SignalInvert(true)
+
+	err = handle.Rts(false)
+	assert.NoError(t, err)
+
+	// By Default its high
+	val, err := handle.Ring()
+	assert.NoError(t, err)
+	assert.Equal(t, false, val)
+
+	err = handle.Rts(true)
+	assert.NoError(t, err)
+
+	time.Sleep(100 * time.Millisecond) // Minimum Wait Time
+
+	// Signal Low
+	val, err = handle.Ring()
+	assert.NoError(t, err)
+	assert.Equal(t, true, val)
+
+	err = handle.Rts(false)
+	assert.NoError(t, err)
+
+	time.Sleep(100 * time.Millisecond) // Minimum Wait Time
+
+	val, err = handle.Ring()
+	assert.NoError(t, err)
+	assert.Equal(t, false, val)
+}
+
+func TestSerialIntegration_P15(t *testing.T) {
+
+	verifySetup(t, paramLOOPBACK)
+
+	handle, err := createPort(t, ParityNone, StopBits1, FlowNone)
+	defer closePort(t, handle)
+
+	// Flip Polarity
+	handle.SignalInvert(true)
+
+	err = handle.Dtr(false)
+	assert.NoError(t, err)
+
+	// By Default its high
+	val, err := handle.Dsr()
+	assert.NoError(t, err)
+	assert.Equal(t, false, val)
+
+	err = handle.Dtr(true)
+	assert.NoError(t, err)
+
+	time.Sleep(100 * time.Millisecond) // Minimum Wait Time
+
+	// Signal Low
+	val, err = handle.Dsr()
+	assert.NoError(t, err)
+	assert.Equal(t, true, val)
+
+	err = handle.Dtr(false)
+	assert.NoError(t, err)
+
+	time.Sleep(100 * time.Millisecond) // Minimum Wait Time
+
+	val, err = handle.Dsr()
+	assert.NoError(t, err)
+	assert.Equal(t, false, val)
+}
+
+func TestSerialIntegration_P16(t *testing.T) {
+
+	verifySetup(t, paramLOOPBACK)
+
+	handle, err := createPort(t, ParityNone, StopBits1, FlowNone)
+	defer closePort(t, handle)
+
+	// Write Some Data
+	buf := []byte("1 2 3 4 5 6 7 8 9 10")
+	writePort(t, handle, buf)
+	time.Sleep(500 * time.Millisecond)
+
+	// Set the Break Signal
+	err = handle.SendBreak(true)
+	assert.NoError(t, err)
+
+	time.Sleep(100 * time.Millisecond) // Minimum Wait Time
+
+	// Clear Break Signal
+	err = handle.SendBreak(false)
+	assert.NoError(t, err)
+
+	time.Sleep(100 * time.Millisecond) // Minimum Wait Time
+
+	// Perform a Read
+	rbuf := make([]byte, len(buf)+10)
+	n, err := handle.Read(rbuf)
+	assert.NoError(t, err)
+	assert.Equal(t, len(buf)+1, n)
+	assert.Equal(t, byte(0), rbuf[len(buf)])
+	t.Logf("Break Signal Read - Size : %d , Data : %q", n, string(rbuf[:n]))
+}
+
+func TestBaudrates(t *testing.T) {
+
+	// loadConfig(t)
+	verifySetup(t, paramLOOPBACK)
+
+	// Test Set
+	ts := []struct {
+		baud     int
+		testSize int
+		hasErr   bool
+		timeout  time.Duration
+	}{
+		{0, 0, true, 0},
+		{baudrate, 1024, false, 1200 * time.Millisecond},
+		{300, 100, false, 5000 * time.Millisecond},
+		{600, 100, false, 3000 * time.Millisecond},
+		{1200, 100, false, 2000 * time.Millisecond},
+		{1800, 100, false, 2000 * time.Millisecond},
+		{2400, 100, false, 2000 * time.Millisecond},
+		{4800, 100, false, 2000 * time.Millisecond},
+		{19200, 1024, false, 1500 * time.Millisecond},
+		{38400, 1024, false, 1200 * time.Millisecond},
+		{57600, 1024, false, 1200 * time.Millisecond},
+		{115200, 1024, false, 1200 * time.Millisecond},
+		{230400, 1024, false, 1200 * time.Millisecond},
+		{460800, 1024, false, 1200 * time.Millisecond},
+		{500000, 1024, false, 1200 * time.Millisecond},
+		{576000, 1024, false, 1200 * time.Millisecond},
+		{921600, 1024, false, 1200 * time.Millisecond},
+		{1000000, 1024, false, 1200 * time.Millisecond},
+		{1152000, 1024, false, 1200 * time.Millisecond},
+		{1500000, 1024, false, 1200 * time.Millisecond},
+		{2000000, 1024, false, 1200 * time.Millisecond},
+		{2500000, 1024, false, 1200 * time.Millisecond},
+		{3000000, 1024, false, 1200 * time.Millisecond},
+		{3500000, 1024, false, 1200 * time.Millisecond},
+		{4000000, 1024, false, 1200 * time.Millisecond},
+		{4500000, 1024, true, 0},
+	}
+
+	// Run Through the Test
+	for _, tc := range ts {
+		name := fmt.Sprintf("Baud Rate %d", tc.baud)
+
+		t.Run(name, func(t *testing.T) {
+
+			// Open Port with Respective BaudRate
+			ref, err := OpenPort(&SerialConfig{
+				Name:     sport,
+				Baud:     tc.baud,
+				Flow:     FlowNone,
+				Parity:   ParityNone,
+				StopBits: StopBits1,
+			})
+
+			// if Error was not Expected
+			if err != nil && !tc.hasErr {
+				t.Errorf("Error in Opening Port - %v", err)
+				t.Fail()
+			}
+
+			// if Error was Expected
+			if err == nil && tc.hasErr {
+				t.Errorf("No Error's Returned even though it was expected")
+				t.Fail()
+			}
+
+			// Log the Error for Information Purpose
+			if err != nil {
+				t.Logf("Info [Err]- %v", err) // Log the Error for Info
+			}
+
+			// Only Run if the test has not failed
+			if !t.Failed() && !tc.hasErr {
+
+				// Seed the Random number Generator
+				rand.Seed(int64(time.Now().Nanosecond()))
+				// Size of the Buffer
+				var arrSize = tc.testSize
+				// Buffer for Transaction
+				buf := make([]byte, arrSize)
+				_, err = rand.Read(buf)
+				if err != nil {
+					t.Errorf("Error create Random Buffer - %v", err)
+					t.Fail()
+				}
+
+				if !t.Failed() {
+					// Perform the Write
+					n, err := ref.Write(buf)
+
+					if err != nil {
+						t.Errorf("Error Write Buffer - %v", err)
+						t.Fail()
+					}
+
+					if n != arrSize {
+						t.Errorf("Expected %d got %d", arrSize, n)
+						t.Fail()
+					}
+				}
+
+				if !t.Failed() {
+					t.Logf("Engage Sleep of %v", tc.timeout)
+					time.Sleep(tc.timeout)
+				}
+
+				if !t.Failed() {
+					// Create the receive buffer
+					rbuf := make([]byte, arrSize)
+
+					//Perform the actual Read
+					n, err := ref.Read(rbuf)
+					if err != nil {
+						t.Errorf("Error Read Buffer - %v", err)
+						t.Fail()
+					}
+
+					if n != arrSize {
+						t.Errorf("Expected %d got %d", arrSize, n)
+						t.Fail()
+					}
+
+					if !bytes.Equal(buf, rbuf) {
+						t.Errorf("Expected Buffers to be Equal")
+						t.Fail()
+					}
+				}
+
+			}
+
+			// Do A close in case if 'ref' is not Nil
+			if ref != nil {
+				err = ref.Close()
+				if err != nil {
+					t.Errorf("Error Failed to Close Port - %v", err)
+					t.Fail()
+				}
+			}
+		})
+	}
 }
